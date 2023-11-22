1a4793824349d60c1f1e3e62e49636855f87c55e
diego <dih.yokoyama03@hotmail.com>
2023-11-22 14:53:10 -0400

    removi tava dando problema

--- testeTelaEditar.py
+++
@@ -1,9 +0,0 @@
-'''def verificar_nome(self, instance):
-        try:
-            verificarNome = self.nome_verificar_txt
-            if verificarNome == self.nome_verificar_txt:
-                self.verificar_lbl.text = 'Nome valido '
-            elif verificarNome != self.nome_verificar_txt:
-                self.verificar_lbl.text = ' nome invalido'
-        except ValueError:
-            self.verificar_lbl.text = 'Por favor, insira um nome valido.'   '''
